2d8b1b97d559f6c6b50e693e4675c9c7813d8241
alyssa-higdon <81454612+alyssa-higdon@users.noreply.github.com>
2023-01-15 19:51:44 -0800

    did all of canvas stuff, but botton not working

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,6 +4,7 @@ import Form from './Form';
 
 function MyApp(){
   const [characters, setCharacters] = useState([]);
+  <Form handleSubmit = {updateList} />
 
 function removeOneCharacter (index){
   const updated = characters.filter((character, i) => {
@@ -12,6 +13,11 @@ function removeOneCharacter (index){
   setCharacters(updated);
   
 }
+
+function updateList(person){
+  setCharacters([...characters, person]);
+}
+
 return (
   <div className="container">
     <Table characterData={characters} removeCharacter={removeOneCharacter} />
