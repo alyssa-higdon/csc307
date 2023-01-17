c7565fc7da2b0f738e8b4d71a6b23910f303cc97
alyssa-higdon <81454612+alyssa-higdon@users.noreply.github.com>
2023-01-17 13:20:39 -0800

    it works

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,7 +4,7 @@ import Form from './Form';
 
 function MyApp(){
   const [characters, setCharacters] = useState([]);
-  <Form handleSubmit = {updateList} />
+  
 
 function removeOneCharacter (index){
   const updated = characters.filter((character, i) => {
@@ -21,6 +21,7 @@ function updateList(person){
 return (
   <div className="container">
     <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form handleSubmit = {updateList} />
   </div>
 )
 }
